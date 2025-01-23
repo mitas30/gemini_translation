f5729116df494cc1e7d7f2adf23799a21f1895c4
mitas30 <mitage1090@gmail.com>
2025-01-23 14:30:27 +0900

    feat:gemini2.0 can translate english txt.

--- extraction.py
+++ extraction.py
@@ -1,17 +1,40 @@
 import pymupdf4llm
 from pathlib import Path
+import pymupdf
 
-def extract_text(input_pdf_path:Path)->str:
+def extract_text_as_md(input_pdf_path:Path)->str:
     text = pymupdf4llm.to_markdown(input_pdf_path)
     return text
+
+def extract_text(input_file_path:Path,output_file_path:Path):
+    doc = pymupdf.open(input_file_path)
+    with open(output_file_path, "wb") as out:
+        for page in doc: 
+            text = page.get_text().encode("utf8") 
+            out.write(text) 
 
 def write_md(md_text:str,output_md_path:Path):
     with open(output_md_path,'w') as f:
         f.write(md_text)
+        
+def save_extracted_text_as_md(input_pdf_path:Path,output_md_path:Path):
+    output_md_path.parent.mkdir(parents=True,exist_ok=True)
+    md_text=extract_text_as_md(input_pdf_path)
+    write_md(md_text,output_md_path)
+    
+def extract_correct_sequence_from_word_file(docx_path:Path,output_txt_path:Path):
+    """翻訳したいワードファイルから、正しい順序で文章を抽出して、その文章をoutput_txt_pathに保存する。
+
+    Args:
+        docx_path (Path): _description_
+        output_txt_path (Path): _description_
+
+    Returns:
+        str: _description_
+    """
+    pass
 
 if __name__ == "__main__":
-    input_pdf_path = Path(__file__).parent / "data" / "input_pdf" / "part4_thought" / "ch25_Genius.pdf"
-    text = extract_text(input_pdf_path)
-    output_pdf_path = Path(__file__).parent / "data" / "output_md" / "part4_thought" / "ch25_Genius.md"
-    output_pdf_path.parent.mkdir(exist_ok=True,parents=True)
-    write_md(text,output_pdf_path)
+    input_pdf_path = Path(__file__).parent / "data" / "input_pdf" / "deep_utopia/Deep Utopia_3_wednesday.pdf"
+    output_md_path = Path(__file__).parent / "data" / "output_md" / "deep_utopia/Deep Utopia_3_wednesday.md"
+    save_extracted_text_as_md(input_pdf_path,output_md_path)
--- translation.py
+++ translation.py
@@ -1,26 +1,80 @@
 import google.generativeai as genai
-from google.generativeai import GenerativeModel
+from google.generativeai import GenerativeModel,ChatSession
 from dotenv import load_dotenv
-import os,json
+import os,json,time
 from pathlib import Path
+from typing import Literal
 
-def translate_text(model:GenerativeModel,save_file_path:Path):
-    chat = model.start_chat()
-    response=chat.send_message("Translate the following text into English: 你好")
-    with open(save_file_path,'a') as f:
-        json.dump(response.text,f,ensure_ascii=False)
-        
-    for i in range(10):
-        response = chat.send_message()
-        if len(response.text) < 20:
-            break
-        with open(save_file_path,'a') as f:
-            json.dump(response.text,f,ensure_ascii=False)
-    
+def do_first_chat(chat:ChatSession,sf_path:Path,text:str)->tuple[float,int,int]:
+    """最初のチャットを行い、翻訳者の能力を確認する。
 
-if __name__ == "__main__":
+    Args:
+        chat (ChatSession): _description_
+        sf_path (Path): _description_
+        text (str): 翻訳対象のテキスト
+
+    Returns:
+        tuple[float,int,int]: 
+        - take_time: チャットにかかった時間
+        - prompt_token_count: プロンプトのトークン数
+        - candidates_token_count: 返信のトークン数
+    """
+    st=time.time()
+    response=chat.send_message(f"あなたは優れた翻訳者です。これから英語の長文を送るので、できるだけ長く日本語に翻訳してください。\n一度に翻訳できなくても、何回かに分けて完全な翻訳を作成する予定なので、あなたは端折らずに翻訳してください。\nまた、元のmdと同じ構成を保って出力することを心がけてください。\n\n<翻訳対象>\n{text}")
+    with open(sf_path,'x') as f:
+        f.write(response.text+'\n')
+    take_time=time.time()-st
+    usage = response.usage_metadata
+    return take_time,usage.prompt_token_count,usage.candidates_token_count 
+
+def do_repeat_chat(chat:ChatSession,sf_path:Path)->tuple[float,int,int]:
+    st=time.time()
+    response = chat.send_message(f"あなたは優れた翻訳者です。以前の人が翻訳してくれた部分を見て、続きを翻訳してください。\nただし、もし前の人で全文の翻訳が完了している場合は、「completed」とだけ返信してください。")
+    if len(response.text) < 20:
+        print("翻訳が完了しました。")
+        return None
+    with open(sf_path,'a') as f:
+        f.write(response.text+'\n')
+    usage = response.usage_metadata      
+    return time.time()-st,usage.prompt_token_count,usage.candidates_token_count
+
+def setup_genai(use_model:Literal["1.5_flash","1.5_pro","2.0_flash"]):
     load_dotenv()
     genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-    model = genai.GenerativeModel("gemini-1.5-pro")
-    response = model.generate_content("Explain how AI works")
-    print(response.text)
+    if use_model=="1.5_flash":
+        model = genai.GenerativeModel("gemini-1.5-flash")
+    elif use_model=="1.5_pro":
+        model = genai.GenerativeModel("gemini-1.5-pro")
+    elif use_model=="2.0_flash":
+        model = genai.GenerativeModel("gemini-2.0-flash-exp")
+    return model
+
+def translate_text(model:GenerativeModel,save_folder:Path,input_md_path:Path):
+    """input_md_pathのテキストを翻訳し、save_folder以下に保存する。
+
+    Args:
+        model (GenerativeModel): _description_
+        save_folder (Path): _description_
+        input_md_path (Path): _description_
+    """
+    print(f"{input_md_path.name}の翻訳を開始します。")
+    chat = model.start_chat()
+    with open(input_md_path,'r') as f:
+        text = f.read()
+    sf_path=save_folder / input_md_path.name
+    take_time=do_first_chat(chat,sf_path,text)
+    print(f"最初のチャットにかかった時間: {take_time[0]:.2f}秒")
+    for i in range(10):
+        res=do_repeat_chat(chat,sf_path)
+        print(f"{i+2}回目のチャットにかかった時間: {res[0]:.2f}秒")
+        if res is None:
+            break
+    print(f"{input_md_path.name}の翻訳が完了しました。")
+    
+if __name__ == "__main__":
+    model=setup_genai(use_model="1.5_pro")
+    data_folder=Path(__file__).parent / "data"
+    input_md_path = data_folder / "output_md" / "part4_thought/ch21_Problem Solving.md"
+    save_folder=data_folder / "output" / "part4_thought/ch21_Problem Solving.md"
+    save_folder.mkdir(parents=True,exist_ok=True)
+    translate_text(model,save_folder,input_md_path)
